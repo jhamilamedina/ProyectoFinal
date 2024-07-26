2e002e3adb6ed66dca37f80b2108e30aebcb61cf
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-07-26 01:21:19 -0500

    Agrega endpoints para Comentarios

--- API_ENVIA_YA/urls.py
+++ API_ENVIA_YA/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework import routers
-from .api import EmpresaAPIView, UsuarioAPIView, AgenciasLimaAPIView
+from .api import EmpresaAPIView, UsuarioAPIView, AgenciasLimaAPIView, ComentariosAPIViews
 
 urlpatterns = [
     # Endpoint para listar y crear Empresas
     path('api/empresas/', EmpresaAPIView.as_view(), name='empresas-list-create'),
     # Endpoint para obtener, actualizar y eliminar una Empresa
     path('api/empresas/<int:id>/', EmpresaAPIView.as_view(), name='empresas-detail-update-delete'),
-    
+
     # Endpoint para listar y crear usuarios
     path('api/usuarios/', UsuarioAPIView.as_view(), name='usuarios-list-create'),
     # Endpoint para obtener, actualizar y eliminar un usuario
@@ -17,4 +17,9 @@ urlpatterns = [
     path('api/agenciaslima/', AgenciasLimaAPIView.as_view(), name='agenciaslima-list-create'),
     # Endpoint para obtener, actualizar y eliminar una agencia
     path('api/agenciaslima/<int:id>/', AgenciasLimaAPIView.as_view(), name='agenciaslima-detail-update-delete'),
+
+    # Endpoint para listar y crear comentarios
+    path('api/comentarios/', ComentariosAPIViews.as_view(), name='comentarios-list-create'),
+    # Endpoint para obtener, actualizar y eliminar un comenario
+    path('api/comentarios/<int:id>/', ComentariosAPIViews.as_view(), name='comentarios-detail-update-delete'),
 ]
