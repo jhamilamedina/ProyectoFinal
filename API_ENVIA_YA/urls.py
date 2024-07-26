3ed96c91dcfc3739172498c5484b81bcd4cb7eeb
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-07-25 22:35:34 -0500

    Agrega endpoints para Agenciaslima API

--- API_ENVIA_YA/urls.py
+++ API_ENVIA_YA/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from rest_framework import routers
-from .api import EmpresaAPIView, UsuarioAPIView
+from .api import EmpresaAPIView, UsuarioAPIView, AgenciasLimaAPIView
 
 urlpatterns = [
     # Endpoint para listar y crear Empresas
     path('api/empresas/', EmpresaAPIView.as_view(), name='empresas-list-create'),
     # Endpoint para obtener, actualizar y eliminar una Empresa
     path('api/empresas/<int:id>/', EmpresaAPIView.as_view(), name='empresas-detail-update-delete'),
+    
     # Endpoint para listar y crear usuarios
     path('api/usuarios/', UsuarioAPIView.as_view(), name='usuarios-list-create'),
     # Endpoint para obtener, actualizar y eliminar un usuario
     path('api/usuarios/<int:id>/', UsuarioAPIView.as_view(), name='usuarios-detail-update-delete'),
+
+    # Endpoint para listar y crear agencias
+    path('api/agenciaslima/', AgenciasLimaAPIView.as_view(), name='agenciaslima-list-create'),
+    # Endpoint para obtener, actualizar y eliminar una agencia
+    path('api/agenciaslima/<int:id>/', AgenciasLimaAPIView.as_view(), name='agenciaslima-detail-update-delete'),
 ]
