a22f0e875ba52cb57eeba63b72fe477229a0ed2e
Maly <jhamilamedina@gmail.com>
2024-07-29 03:06:06 -0500

    build: Se actualiza el App.js

--- frontend/envia-ya/src/App.js
+++ frontend/envia-ya/src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Destinos from './components/Destinos';
 import Empresas from './components/Empresas';
+import Evaluacion from './components/Evaluacion';
+import EstrellasRating from './components/EstrellasRating';
 import Nosotros from './components/Nosotros';
 import Inicio from './components/Inicio';
 
@@ -17,6 +19,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path='/destinos' element={<Destinos />} />
           <Route path='/empresas' element={<Empresas />} />
+          <Route path='/Evaluacion' element={<Evaluacion />} />
+          <Route path='/EstrellasRating' element={<EstrellasRating />} />
+          <Route path="/rating-pagina" component={() => <div>Valoración...</div>} />
+          <Route path='/mas-evaluaciones' component={() => <div>Más evaluaciones...</div>} />
           <Route path='/Nosotros' element={<Nosotros />} />
           <Route path='/Inicio' element={<Inicio />} />
         </Routes>
