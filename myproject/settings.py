33679c4a8c95003ab04bf2ad32a27dae1783d59e
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-08-05 22:37:52 -0500

    se agrega libreria para generar la documentacion de la api

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'API_ENVIA_YA',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 REST_FRAMEWORK = {
