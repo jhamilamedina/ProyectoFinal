5f6a4a5de7c5b3c1eacd7792c8a1d2f66afde30e
misaelbazan <misaelbfranco@gmail.com>
2024-08-06 19:56:55 -0500

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'API_ENVIA_YA',
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -58,6 +59,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Reemplaza con la URL de tu frontend
 ]
 
 ROOT_URLCONF = 'myproject.urls'
