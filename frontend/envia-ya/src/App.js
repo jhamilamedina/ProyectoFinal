1f51c5c242e234173e45fc65cbee7043523e4736
misaelbazan <misaelbfranco@gmail.com>
2024-08-06 19:54:34 -0500

    Adding my recent changes on Filter.js

--- frontend/envia-ya/src/App.js
+++ frontend/envia-ya/src/App.js
@@ -1,45 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Ayuda from './components/Ayuda';
-import Home from './components/Home';
-import Destinos from './components/Destinos';
-import Empresas from './components/Empresas';
-import Evaluacion from './components/Evaluacion';
-import EstrellasRating from './components/EstrellasRating';
-import Nosotros from './components/Nosotros';
-import Inicio from './components/Inicio';
-import Contacto from './components/Contacto';
-import Privacidad from './components/Privacidad';
+  import React from 'react';
+  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+  import Header from './components/Header';
+  import Footer from './components/Footer';
+  import Ayuda from './components/Ayuda';
+  import Home from './components/Home';
+  import Destinos from './components/Destinos';
+  import Evaluacion from './components/Evaluacion';
+  import EstrellasRating from './components/EstrellasRating';
+  import Nosotros from './components/Nosotros';
+  import Inicio from './components/Inicio';
+  import Contacto from './components/Contacto';
+  import Privacidad from './components/Privacidad';
+  import './App.css'
 
-import './App.css'
-
-function App() {
-  return (
-    <Router>
-      <div className='app-container'>
-      <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/destinos' element={<Destinos />} />
-            <Route path='/empresas' element={<Empresas />} />
-            <Route path='/Evaluacion' element={<Evaluacion />} />
-            <Route path='/EstrellasRating' element={<EstrellasRating />} />
-            <Route path="/rating-pagina" component={() => <div>Valoración...</div>} />
-            <Route path='/mas-evaluaciones' component={() => <div>Más evaluaciones...</div>} />
-            <Route path='/Nosotros' element={<Nosotros />} />
-            <Route path='/Inicio' element={<Inicio />} />
-            <Route path='/Ayuda' element={<Ayuda />} />
-            <Route path='/Contacto' element={<Contacto />} />
-            <Route path='/Privacidad' element={<Privacidad />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <div className='app-container'>
+        <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path='/destinos' element={<Destinos />} />
+              <Route path='/Evaluacion' element={<Evaluacion />} />
+              <Route path='/EstrellasRating' element={<EstrellasRating />} />
+              <Route path="/rating-pagina" element={<div>Valoracion...</div>} />
+              <Route path='/mas-evaluaciones' element={<div>Más evaluaciones...</div>} />
+              <Route path='/Nosotros' element={<Nosotros />} />
+              <Route path='/Inicio' element={<Inicio />} />
+              <Route path='/Ayuda' element={<Ayuda />} />
+              <Route path='/Contacto' element={<Contacto />} />
+              <Route path='/Privacidad' element={<Privacidad />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 
 export default App;
