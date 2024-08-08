b1b7c999136226f6d89974fd3a3f49bb64549c66
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-08-07 20:19:23 -0500

    se actualizo para subir imagenes

--- myproject/urls.py
+++ myproject/urls.py
@@ -45,7 +45,6 @@ urlpatterns = [
     path('', include('API_ENVIA_YA.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
